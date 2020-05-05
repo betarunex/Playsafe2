a73f677f0e6753a4e18ed52b4fd2bb665bae411e
betarunex <betarunex@gmail.com>
2020-05-05 13:47:55 +0200

    Part 2 of playsafe testing

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // read file
+        // run game
+            // thread show game results
+            // main thread take user input
+        // end game
+    }
+}
